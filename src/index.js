532544b417a810b56bb1ee5eb2b1ad944fc1b781
lâm thế huân <huantyn@gmail.com>
2024-11-28 14:00:58 +0700

    chore(api): prepare folder structureand routes for API versioning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const v1Router = require('./v1/routes')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -8,6 +10,10 @@ const PORT = process.env.PORT || 3000
 app.get("/", (req, res) => {
   res.send("<h2>It's working!</h2>")
 })
+
+
+// ADD
+app.use("/api/v1", v1Router)
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`)
